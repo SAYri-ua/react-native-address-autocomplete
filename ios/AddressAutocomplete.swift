0700c6b5253d0b1fd4bda9897097f5648990ed21
SAYri-ua <sayri.ua@gmail.com>
2025-09-17 00:37:35 +0300

    refactor: streamline address suggestion and detail methods in AddressAutocomplete

--- ios/AddressAutocomplete.swift
+++ ios/AddressAutocomplete.swift
@@ -1,95 +1,130 @@
-import MapKit
 import Foundation
+import MapKit
+import CoreLocation
+import React
 
 @objc(AddressAutocomplete)
 class AddressAutocomplete: NSObject, MKLocalSearchCompleterDelegate {
 
-    var resolver: RCTPromiseResolveBlock?
-    var rejecter: RCTPromiseRejectBlock?
-    let completer: MKLocalSearchCompleter = MKLocalSearchCompleter();
-    var searchRequest: MKLocalSearch.Request?
-    var localSearch: MKLocalSearch?
+  private let completer = MKLocalSearchCompleter()
+  private var suggestionsResolve: RCTPromiseResolveBlock?
+  private var suggestionsReject: RCTPromiseRejectBlock?
 
-    override init() {
-        super.init()
-        completer.delegate = self
+  override init() {
+    super.init()
+    completer.delegate = self
+  }
+
+  // MARK: - JS API (селектори мають збігатись із .m)
+
+  // AddressAutocomplete.m:
+  // RCT_EXTERN_METHOD(getAddressSuggestions:(NSString *)address
+  //                  withResolver:(RCTPromiseResolveBlock)resolve
+  //                  withRejecter:(RCTPromiseRejectBlock)reject))
+  @objc(getAddressSuggestions:withResolver:withRejecter:)
+  func getAddressSuggestions(_ address: NSString,
+                             withResolver resolve: @escaping RCTPromiseResolveBlock,
+                             withRejecter reject: @escaping RCTPromiseRejectBlock) {
+    suggestionsResolve = resolve
+    suggestionsReject = reject
+    completer.queryFragment = address as String
+  }
+
+  // AddressAutocomplete.m:
+  // RCT_EXTERN_METHOD(getAddressDetails:(NSString *)address
+  //                  withResolver:(RCTPromiseResolveBlock)resolve
+  //                  withRejecter:(RCTPromiseRejectBlock)reject))
+  @objc(getAddressDetails:withResolver:withRejecter:)
+  func getAddressDetails(_ address: NSString,
+                         withResolver resolve: @escaping RCTPromiseResolveBlock,
+                         withRejecter reject: @escaping RCTPromiseRejectBlock) {
+    let request = MKLocalSearch.Request()
+    request.naturalLanguageQuery = address as String
+
+    let search = MKLocalSearch(request: request)
+    search.start { response, error in
+      if let error = error {
+        reject("address_details", error.localizedDescription, error)
+        return
+      }
+
+      guard let item = response?.mapItems.first,
+            let loc = item.placemark.location else {
+        // Можеш повертати null або кидати помилку — на твій розсуд
+        resolve(NSNull())
+        return
+      }
+
+      let result: [String: Any] = [
+        "latitude": loc.coordinate.latitude,
+        "longitude": loc.coordinate.longitude,
+        "name": item.name ?? "",
+        "title": item.placemark.title ?? ""
+      ]
+      resolve(result)
     }
-    
-    @objc(getAddressSuggestions:withResolver:withRejecter:)
-    func getAddressSuggestions(address: String!, resolve: @escaping RCTPromiseResolveBlock, reject: @escaping RCTPromiseRejectBlock) {
-        DispatchQueue.main.async {
-            if self.completer.isSearching {
-                self.completer.cancel()
-            }
-            self.completer.queryFragment = address
-            self.resolver = resolve
-            self.rejecter = reject
+  }
 
-            if self.completer.isSearching {
-                print("Searching" + address) // Prints
-            }
-        }
+  // AddressAutocomplete.m:
+  // RCT_EXTERN_METHOD(reverseGeocodeLocation:(NSNumber *)longitude
+  //                  withLatitude:(NSNumber *)latitude
+  //                  withResolver:(RCTPromiseResolveBlock)resolve
+  //                  withRejecter:(RCTPromiseRejectBlock)reject))
+  @objc(reverseGeocodeLocation:withLatitude:withResolver:withRejecter:)
+  func reverseGeocodeLocation(_ longitude: NSNumber,
+                              withLatitude latitude: NSNumber,
+                              withResolver resolve: @escaping RCTPromiseResolveBlock,
+                              withRejecter reject: @escaping RCTPromiseRejectBlock) {
+
+    let location = CLLocation(latitude: latitude.doubleValue,
+                              longitude: longitude.doubleValue)
+
+    CLGeocoder().reverseGeocodeLocation(location) { placemarks, error in
+      if let error = error {
+        reject("reverse_geocode", error.localizedDescription, error)
+        return
+      }
+
+      guard let p = placemarks?.first else {
+        resolve(NSNull())
+        return
+      }
+
+      var result: [String: Any] = [
+        "latitude": location.coordinate.latitude,
+        "longitude": location.coordinate.longitude,
+      ]
+      if let name = p.name { result["name"] = name }
+      if let city = p.locality { result["city"] = city }
+      if let street = p.thoroughfare { result["street"] = street }
+      if let house = p.subThoroughfare { result["streetNumber"] = house }
+      if let postal = p.postalCode { result["postalCode"] = postal }
+      if let iso = p.isoCountryCode { result["isoCountryCode"] = iso }
+
+      resolve(result)
     }
+  }
 
-    @objc(getAddressDetails:withResolver:withRejecter:)
-    func getAddressDetails(address: String!, resolve: @escaping RCTPromiseResolveBlock, reject: @escaping RCTPromiseRejectBlock) {
-        DispatchQueue.main.async {
-            if self.localSearch?.isSearching == true {
-                print("Canceling request")
-                self.localSearch?.cancel()
-            }
-            self.searchRequest = MKLocalSearch.Request()
-            self.searchRequest?.naturalLanguageQuery = address
-            self.localSearch = MKLocalSearch(request: self.searchRequest!);
-            
-            self.localSearch?.start { (response, error) in
-                guard let response = response else {
-                    print("Error: \(error?.localizedDescription ?? "Unknown error").")
-                    self.rejecter?("address_autocomplete", "Unknown error", error)
-                    return
-                }
+  // MARK: - MKLocalSearchCompleterDelegate
 
-                let items = response.mapItems;
-                let item = items[0]
-                
-                let details:[String: Any] = [
-                    "title": item.placemark.title! as String,
-                    "coordinate": [
-                        "longitude": item.placemark.coordinate.longitude as Double,
-                        "latitude": item.placemark.coordinate.latitude as Double
-                    ],
-                    "region": [
-                        "longitude": response.boundingRegion.center.longitude as Double,
-                        "latitude": response.boundingRegion.center.latitude as Double,
-                        "latitudeDelta": response.boundingRegion.span.latitudeDelta,
-                        "longitudeDelta": response.boundingRegion.span.longitudeDelta
-                    ]
-                ]
-                
-                resolve(details)
-            }
-        }
+  func completerDidUpdateResults(_ completer: MKLocalSearchCompleter) {
+    let items = completer.results.map { res in
+      ["title": res.title, "subtitle": res.subtitle]
     }
+    suggestionsResolve?(items)
+    suggestionsResolve = nil
+    suggestionsReject = nil
+  }
 
-    func completerDidUpdateResults(_ completer: MKLocalSearchCompleter) {
-        let results = completer.results.flatMap { (result) -> String? in
-            return result.title + " " + result.subtitle
-        }
-        print(completer.results) // Prints
-        self.resolver?(results)
-    }
+  func completer(_ completer: MKLocalSearchCompleter, didFailWithError error: Error) {
+    suggestionsReject?("address_autocomplete", error.localizedDescription, error)
+    suggestionsResolve = nil
+    suggestionsReject = nil
+  }
 
-    func completer(_ completer: MKLocalSearchCompleter, didFailWithError error: Error) {
-        self.rejecter?("address_autocomplete", "An error occured", error)
-        print(error)
-    }
-
-    @objc(multiply:withB:withResolver:withRejecter:)
-    func multiply(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
-        resolve(a*b)
-    }
-
-    @objc static func requiresMainQueueSetup() -> Bool {
-        return false
-    }
+  // MARK: - RN requirement (класовий метод!)
+  @objc static func requiresMainQueueSetup() -> Bool {
+    // Використовуємо MapKit/CoreLocation — безпечніше ініціалізувати на main queue
+    return true
+  }
 }
